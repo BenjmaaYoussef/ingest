1fce90798b190e04e37d1d104d028a5b7b40fe06
BenjmaaYoussef <benjmaay23@gmail.com>
2024-04-09 17:03:36 +0100

    Added ga4

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-KFL7J0MRYT"
+      ></Script>
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KFL7J0MRYT');`}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
